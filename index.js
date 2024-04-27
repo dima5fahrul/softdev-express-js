11eb284a24c6130ade185f7555661e2d021e6e2d
Dimas Fahrul <dimasfahrul@Dimass-MacBook-Air.local>
2024-04-27 21:04:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/tshirt', (req, res) => {
+  res.status(200).send({
+    tshirt: '👕',
+    size: 'large'
+  });
+});
+
+app.post('/tshirt/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!' });
+  }
+
+  res.send({
+    tshirt: `👕 with your ${logo} and ID of ${id}`,
+  }); 
+});
+
+app.listen(
+  PORT, () => console.log(`Server is running on port  http://localhost:${PORT}`)
+);
